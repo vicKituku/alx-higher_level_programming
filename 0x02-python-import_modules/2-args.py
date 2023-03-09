9eb87f2d54fff885687a7dba70502b35a2ae53d9
vicKituku <victorkituku@gmail.com>
2023-03-09 01:39:05 -0800

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author: Victor Mwangangi
+import sys
+arguments = len(sys.argv)
+if arguments == 1:
+    print("0 arguments.")
+elif arguments == 2:
+    print("{} argument:".format(arguments-1))
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(arguments-1))
+    for index in range(arguments-1):
+        print("{}: {}".format(index+1, sys.argv[index+1]))
