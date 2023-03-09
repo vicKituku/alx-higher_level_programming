5f079d67483bf30c033d7e8c39e8d21bd165b327
vicKituku <victorkituku@gmail.com>
2023-03-09 02:39:19 -0800

    hidden discory

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Author: Victor Mwangangi
-if __name__ = "__main__":
+if __name__ == "__main__":
     import hidden_4
     for name in dir(hidden_4):
         if name[:2] != "__":
