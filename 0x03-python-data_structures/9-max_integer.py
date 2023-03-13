1d9ddd0415ccfa856049f6cbcbf174de9c13b7c4
vicKituku <victorkituku@gmail.com>
2023-03-13 02:14:03 -0700

    max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author: Victor Mwangangi
+
+def max_integer(my_list=[]):
+    max_value = 0
+    if my_list == []:
+        return None
+    else:
+        for i in my_list:
+            if i > max_value:
+                max_value = i
+    return max_value
