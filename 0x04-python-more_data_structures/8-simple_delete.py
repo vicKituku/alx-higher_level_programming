35d0a56f5350e32ed0bbe3b588239e2a78d7d26f
vicKituku <victorkituku@gmail.com>
2023-03-15 04:24:31 -0700

    simple delete

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: Victor Mwangangi
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary.keys():
+        del a_dictionary[key]
+        return a_dictionary
+    else:
+        return a_dictionary
