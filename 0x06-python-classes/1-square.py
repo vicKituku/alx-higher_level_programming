a5f883a8c8d8006becd45875bda22629cfcf04f2
vicKituku <victorkituku@gmail.com>
2023-03-28 02:43:26 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,5 +4,11 @@
 
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new Square
+        Args:
+            size(int): size of the square
+        """
         self.__size = size
