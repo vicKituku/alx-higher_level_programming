0f52bc177e760c099d71af1d392fe9b89ae0c05e
vicKituku <victorkituku@gmail.com>
2023-04-03 13:37:03 -0700

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        type(self).number_of_instances += 0
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -56,5 +56,5 @@ class Rectangle:
         return ("Rectangle({}, {})".format(self.__width, self.__height))
 
     def __del__(self):
-        type(self).number_of_instances += 0
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
