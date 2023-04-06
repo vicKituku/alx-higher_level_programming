95a95b0c5a79c90a9491f7a83e914344479728f4
vicKituku <victorkituku@gmail.com>
2023-04-06 00:16:27 -0700

    pycodestyle fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that prints 2 new lines 
+"""Defines a function that prints 2 new lines
 after certain characters"""
 
 
@@ -10,23 +10,22 @@ def text_indentation(text):
     # Check if the input is a string
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     # Remove any leading or trailing whitespace
     text = text.strip()
-    
+
     # Initialize the result string
     result = ""
-    
+
     # Loop over each character in the text
     for i in range(len(text)):
         # Append the current character to the result string
         result += text[i]
-        
+
         # Check if the current character is one of ., ? or :
         if text[i] in (".", "?", ":"):
             # Add 2 new lines after the character
             result += "\n\n"
-    
+
     # Print the result string
     print(result)
-
