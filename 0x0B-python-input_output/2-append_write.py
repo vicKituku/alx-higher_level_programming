b30265e9b0fd8c39cf60833c43630f8e61ee3a0c
vicKituku <victorkituku@gmail.com>
2023-04-12 19:06:56 -0700

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function that appends a string at the end of a text
+file (UTF8) and returns the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text
+    file (UTF8) and returns the number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as textfile:
+        return textfile.write(text)
