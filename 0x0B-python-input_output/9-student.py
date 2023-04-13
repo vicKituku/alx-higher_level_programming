b832869c78657dd7d146976bb10554d9be13e1b9
vicKituku <victorkituku@gmail.com>
2023-04-12 19:37:14 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines  a class Student that defines a
+student by firdst name, last name and age"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
