8c5379cf724a93b905f542bf26edc0ad821925e1
vicKituku <victorkituku@gmail.com>
2023-04-20 18:42:47 +0300

    test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,60 +1,98 @@
 #!/usr/bin/python3
 """
-Unit tests for base.py
+Defines unittests for base.py
 """
-
-import unittest
 import os
+import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """Test cases for the Base class"""
+class TestBaseInstantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
 
-    def test_id(self):
-        """Test assigning and incrementing of id attribute"""
+    def test_no_arg(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base(12)
-        self.assertEqual(b3.id, 12)
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
+        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_to_json_string(self):
-        """Test conversion of dictionary to json string"""
-        r1 = {"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}
-        r2 = {"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}
-        json_string = Base.to_json_string([r1, r2])
-        self.assertEqual(json_string, '[{"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}, '
-                                       '{"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}]')
-        self.assertEqual(Base.to_json_string(None), '[]')
-        self.assertEqual(Base.to_json_string([]), '[]')
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
 
-    def test_save_to_file(self):
-        """Test saving of json string to file"""
-        r1 = {"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}
-        r2 = {"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}
-        Base.save_to_file([Base(**r1), Base(**r2)])
-        with open("Base.json", "r") as file:
-            content = file.read()
-            self.assertEqual(content, '[{"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}, '
-                                       '{"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}]')
-        os.remove("Base.json")
+    def test_None_id(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_from_json_string(self):
-        """Test conversion of json string to dictionary"""
-        json_string = '[{"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}, '
-        '{"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}]'
-        expected = [{"id": 1, "width": 5, "height": 10, "x": 2, "y": 3},
-                    {"id": 2, "width": 10, "height": 15, "x": 0, "y": 0}]
-        self.assertEqual(Base.from_json_string(json_string), expected)
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(""), [])
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
 
-    def test_create(self):
-        """Test creation of instance from dictionary"""
-        r1 = {"id": 1, "width": 5, "height": 10, "x": 2, "y": 3}
-        new = Base.create(**
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
 
+    def test_id_public(self):
+        b = Base(12)
+        b.id = 15
+        self.assertEqual(15, b.id)
+
+    def test_nb_instances_private(self):
+        with self.assertRaises(AttributeError):
+            Base(12)._Base__nb_instances
+
+    def test_str_id(self):
+        self.assertEqual("hello", Base("hello").id)
+
+    def test_float_id(self):
+        self.assertEqual(5.5, Base(5.5).id)
+
+    def test_complex_id(self):
+        self.assertEqual(complex(5), Base(complex(5)).id)
+
+    def test_dict_id(self):
+        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+
+    def test_bool_id(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_list_id(self):
+        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+
+    def test_tuple_id(self):
+        self.assertEqual((1, 2), Base((1, 2)).id)
+
+    def test_set_id(self):
+        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+
+    def test_frozenset_id(self):
+        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
+
+    def test_range_id(self):
+        self.assertEqual(range(5), Base(range(5)).id)
+
+    def test_bytes_id(self):
+        self.assertEqual(b'Python', Base(b'Python').id)
+
+    def test_bytearray_id(self):
+        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
+
+    def test_memoryview_id(self):
+        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
+
+    def test_inf_id(self):
+        self.assertEqual(float('inf'), Base(float('inf')).id)
+
+    def test_NaN_id(self):
+        self.assertNotEqual(float('nan'), Base(float('nan')).id)
+
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
+if __name__ == '__main__':
+    unittest.main()
