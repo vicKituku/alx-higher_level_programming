d7df96c9de1bac1bebdef8a8eada7d8e9c621e06
vicKituku <victorkituku@gmail.com>
2023-07-20 18:03:22 +0300

    15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Display number of records with the same score in the table second_table in descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
