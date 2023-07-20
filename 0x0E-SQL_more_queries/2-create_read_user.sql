e3a3cb6291f684877076e2633838aea7db7abc56
vicKituku <victorkituku@gmail.com>
2023-07-20 18:22:29 +0300

    2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to created user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
